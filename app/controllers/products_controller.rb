7a292a5e9e255ff6d9246cffdfb93cefd6ee5030
TomokiCHIBA <michiamo.t1@gmail.com>
2026-01-10 15:52:22 +0000

    feat: 編集機能の追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,8 +20,21 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
     def product_params
-      params.expect(product: [:name])
+      params.expect(product: [ :name ])
     end
 end
