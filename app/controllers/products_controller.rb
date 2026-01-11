8d81e52e2f2889047eb37f75cb1882fbddf32d73
TomokiCHIBA <michiamo.t1@gmail.com>
2026-01-11 08:07:35 +0000

    feat: 削除機能の追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: %i[ show edit update]
+  before_action :set_product, only: %i[ show edit update destroy ]
 
   def index
     @products = Product.all
@@ -32,9 +32,14 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
   private
     def set_product
-      @product = Product.find(params[:ids])
+      @product = Product.find(params[:id])
     end
 
     def product_params
